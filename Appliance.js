b505c968da429dff46fdea62566d7e8b6c5ddeff
shakeel <shakeelanver26@gmail.com>
2020-07-25 21:52:29 +1000

    created an appliance class that stores all information about each appliance added by the user

---
+++ Appliance.js
@@ -0,0 +1,7 @@
+class Appliance {
+    constructor(name, power_rating, daily_consumed) {
+        this.name = name;
+        this.power_rating = power_rating;
+        this.daily_consumed = daily_consumed;
+    }
+}
