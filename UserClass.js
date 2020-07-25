e2b8473c73401693875d122f9e1382e40d5feab2
shakeel <shakeelanver26@gmail.com>
2020-07-25 21:41:42 +1000

    changed this file from data.js to userclass.js to make it a file that stores the user class

---
+++ UserClass.js
@@ -0,0 +1,16 @@
+
+
+//User class implementation
+class User {
+    //user inputs name, email and password during registration 
+    constructor(name, email,password, appliances, goal, daily_use, weekly_use) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.appliances = appliances;
+        this.goal = goal;
+        this.daily_use = daily_use;
+        this.weekly_use = weekly_use;
+    }
+}
+
