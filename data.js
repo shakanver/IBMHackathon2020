58636d10d3a30431f6f8b0a2354e79793c14f63d
shakeel <shakeelanver26@gmail.com>
2020-07-25 22:14:41 +1000

    created a data file to store all app data during the demo, NOT actual database implementation

---
+++ data.js
@@ -0,0 +1,15 @@
+//for the purpose of this demo, this is where we will store all our registered users
+//and appliances created in simple lists
+//For the future, we plan to use secure database systems such as MongoDB
+
+data = [{
+    "users": [],
+    "numUsers": 0,
+    "passwords": [],
+    "emails": [],
+    },
+    {
+     "appliances": [],
+     "numAppliances": 0,
+    }
+];
