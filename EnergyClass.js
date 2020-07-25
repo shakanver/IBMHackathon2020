a3d35b72524115d7f70d9be4c644c2dd2242d2c0
shakeel <shakeelanver26@gmail.com>
2020-07-25 21:48:30 +1000

    created an energy class that stores all energy consumption and goal data

---
+++ EnergyClass.js
@@ -0,0 +1,9 @@
+//energy class that stores the energy data for each user
+class Energy {
+    constructor(daily_goal, weekly_goal, daily_used, weekly_used) {
+        this.daily_goal = daily_goal;
+        this.weekly_goal = weekly_goal;
+        this.daily_used = daily_used;
+        this.weekly_used = weekly_used;
+    }
+}
